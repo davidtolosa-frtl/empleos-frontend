91f733a50a63c1422d43199922edbb7ead12e722
David Tolosa <davidtolosa5@gmail.com>
2025-09-30 20:44:28 -0300

    Leer url api desde variables de entorno

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// Configurar la URL base de tu backend
+// Configurar la URL base de tu backend desde variable de entorno
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
